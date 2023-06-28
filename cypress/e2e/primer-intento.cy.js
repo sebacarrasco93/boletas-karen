e71838474a396aa0e11e585d67ae02d1059753b4
Seba Carrasco Poblete <sebacarrasco93@gmail.com>
2023-06-28 04:54:45 -0400

    Casi casi listo!

--- cypress/e2e/primer-intento.cy.js
+++ cypress/e2e/primer-intento.cy.js
@@ -1,29 +1,32 @@
 describe('Primer intente', () => {
+  it(`Subiendo`, () => {
+    // cy.task('obtenerArchivosEnCarpeta', 'DEJAR_BOLETAS').then((archivos) => {
+      
+    // });
 
-  const boletas = ['Kinesióloga'];
+    cy.visit('https://nuevamasvida.cl');
+    cy.get('#rut')
+      .type(Cypress.env('RUT'));
+    cy.get('#clave')
+      .type(Cypress.env('CLAVE'));
+    cy.get('[value="Ingresar"]')
+      .click();
+    cy.task('obtenerArchivosEnCarpeta', 'DEJAR_BOLETAS').then((archivos) => {
+      archivos.forEach((archivo) => {
+        cy.log(`Procesando ${archivo}`);
 
-  boletas.forEach((archivo) => {
-    it(`Intentando subir ${archivo}`, () => {
-      cy.task('obtenerArchivosEnCarpeta', 'DEJAR_BOLETAS').then((archivos) => {
-        archivos.forEach((archivo) => {
-          const arrayDatos = archivo.split('-');
-          const rutDocumento = arrayDatos[0].replace('bhe_', '');
-          const folioDocumento = arrayDatos[1];
+        const arrayDatos = archivo.split('-');
+        const rutDocumento = arrayDatos[0].replace('bhe_', '');
+        const folioDocumento = arrayDatos[1];
+
+        const rutEncontrado = Cypress.env('DATOS_PROFESIONALES').find((profesional) => {
+          return profesional.RUT_PROFESIONAL.split('-')[0] == rutDocumento;
+        });
+
+        if (rutEncontrado) {
+          cy.log(`Se encontró el RUT de ${rutEncontrado.NOMBRE_PROFESIONAL}`);
           cy.log(`RUT ${rutDocumento}`);
           cy.log(`Folio ${folioDocumento}`);
-        });
-      });
-
-      cy.visit('https://nuevamasvida.cl');
-      cy.get('#rut')
-        .type(Cypress.env('RUT'));
-      cy.get('#clave')
-        .type(Cypress.env('CLAVE'));
-      cy.get('[value="Ingresar"]')
-        .click();
-      cy.task('obtenerArchivosEnCarpeta', 'DEJAR_BOLETAS').then((archivos) => {
-        archivos.forEach((archivo) => {
-          cy.log(`Procesando ${archivo}`);
 
           cy.visit('https://sv.nuevamasvida.cl/sucursal_virtual/solicitud_reembolso.php');
           cy.get('#chk_verificacion')
@@ -35,21 +38,21 @@ describe('Primer intente', () => {
           cy.get('#srw_tipo_consulta')
             .select('Consulta médica');
           cy.get('#ptd_rut')
-            .type('18376588-4');
+            .type(rutEncontrado.RUT_PROFESIONAL);
           cy.get('#srw_num_boleta')
-            .type('123');
+            .type(folioDocumento);
           cy.get('#continuar_datos')
             .click();
           cy.get('#srw_rut_tratante')
-            .type('18376588-4');
-          cy.get('#srw_nom_tratante')
-            .type('Sebastián Carrasco Poblete')
+            .type(rutEncontrado.RUT_PROFESIONAL);
+          // cy.get('#srw_nom_tratante')
+          //   .type('Sebastián Carrasco Poblete')
 
           cy.get('#archivo').selectFile(`DEJAR_BOLETAS/${archivo}`, { force: true });
-          cy.get('#archivo_oa').selectFile(`DEJAR_BOLETAS/${archivo}`, { force: true });
+          // cy.get('#archivo_oa').selectFile(`DEJAR_BOLETAS/${archivo}`, { force: true });
           cy.get('#chk_verificacion')
             .click();
-        });
+        }
       });
     });
   });
