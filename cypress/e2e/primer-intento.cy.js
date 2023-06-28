562ba71a978c9d8624fdf41f38461ee6857e1e48
Seba Carrasco Poblete <sebacarrasco93@gmail.com>
2023-06-27 23:43:25 -0400

    Agregar forEach

--- cypress/e2e/primer-intento.cy.js
+++ cypress/e2e/primer-intento.cy.js
@@ -1,35 +1,40 @@
 describe('Primer intente', () => {
 
-  it('intento 1', () => {
-    cy.visit('https://nuevamasvida.cl');
-    cy.get('#rut')
-      .type(Cypress.env('RUT'));
-    cy.get('#clave')
-      .type(Cypress.env('CLAVE'));
-    cy.get('[value="Ingresar"]')
-      .click();
-    cy.visit('https://sv.nuevamasvida.cl/sucursal_virtual/solicitud_reembolso.php');
-    cy.get('#chk_verificacion')
-      .click();
-    cy.get('#activo > img')
-      .click();
-    cy.get('#bnf_rut')
-      .select(Cypress.env('NOMBRE'));
-    cy.get('#srw_tipo_consulta')
-      .select('Consulta médica');
-    cy.get('#ptd_rut')
-      .type('18376588-4');
-    cy.get('#srw_num_boleta')
-      .type('123');
-    cy.get('#continuar_datos')
-      .click();
-    cy.get('#srw_rut_tratante')
-      .type('18376588-4');
-    cy.get('#srw_nom_tratante')
-      .type('Sebastián Carrasco Poblete')
-    cy.get('#archivo').selectFile('DEJAR_BOLETAS/json.json', { force: true });
-    cy.get('#archivo_oa').selectFile('DEJAR_BOLETAS/json.json', { force: true });
-    cy.get('#chk_verificacion')
-      .click();
-  })
-})
+  const boletas = ['Kinesióloga', 'Fonoaudiólogo', 'Otra consulta'];
+
+  boletas.forEach((archivo) => {
+
+    it(`Intentando subir ${archivo}`, () => {
+      cy.visit('https://nuevamasvida.cl');
+      cy.get('#rut')
+        .type(Cypress.env('RUT'));
+      cy.get('#clave')
+        .type(Cypress.env('CLAVE'));
+      cy.get('[value="Ingresar"]')
+        .click();
+      cy.visit('https://sv.nuevamasvida.cl/sucursal_virtual/solicitud_reembolso.php');
+      cy.get('#chk_verificacion')
+        .click();
+      cy.get('#activo > img')
+        .click();
+      cy.get('#bnf_rut')
+        .select(Cypress.env('NOMBRE'));
+      cy.get('#srw_tipo_consulta')
+        .select('Consulta médica');
+      cy.get('#ptd_rut')
+        .type('18376588-4');
+      cy.get('#srw_num_boleta')
+        .type('123');
+      cy.get('#continuar_datos')
+        .click();
+      cy.get('#srw_rut_tratante')
+        .type('18376588-4');
+      cy.get('#srw_nom_tratante')
+        .type('Sebastián Carrasco Poblete')
+      cy.get('#archivo').selectFile('DEJAR_BOLETAS/json.json', { force: true });
+      cy.get('#archivo_oa').selectFile('DEJAR_BOLETAS/json.json', { force: true });
+      cy.get('#chk_verificacion')
+        .click();
+    });
+  });
+});
