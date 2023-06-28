794f2f8ddc2a5273282cbf0b1f616b1178097c98
Seba Carrasco Poblete <sebacarrasco93@gmail.com>
2023-06-28 01:57:12 -0400

    Agregar task para leer archivos PDF en carpeta!

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,24 @@
 const { defineConfig } = require("cypress");
+const fs = require('fs');
+const path = require('path');
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
+      on('task', {
+        obtenerArchivosEnCarpeta(carpeta) {
+          const rutaCarpeta = path.join(__dirname, carpeta);
+          const archivos = fs.readdirSync(carpeta);
+
+          const archivosPDF = archivos.filter((archivo) => {
+            const extension = path.extname(archivo).toLowerCase();
+            return extension === '.pdf';
+          });
+            
+          return archivosPDF;
+        },
+      });
+      
       // implement node event listeners here
     },
   },
--- cypress/e2e/primer-intento.cy.js
+++ cypress/e2e/primer-intento.cy.js
@@ -1,9 +1,10 @@
+let fs = require("fs");
+
 describe('Primer intente', () => {
 
-  const boletas = ['Kinesióloga', 'Fonoaudiólogo', 'Otra consulta'];
+  const boletas = ['Kinesióloga'];
 
   boletas.forEach((archivo) => {
-
     it(`Intentando subir ${archivo}`, () => {
       cy.visit('https://nuevamasvida.cl');
       cy.get('#rut')
@@ -12,29 +13,36 @@ describe('Primer intente', () => {
         .type(Cypress.env('CLAVE'));
       cy.get('[value="Ingresar"]')
         .click();
-      cy.visit('https://sv.nuevamasvida.cl/sucursal_virtual/solicitud_reembolso.php');
-      cy.get('#chk_verificacion')
-        .click();
-      cy.get('#activo > img')
-        .click();
-      cy.get('#bnf_rut')
-        .select(Cypress.env('NOMBRE'));
-      cy.get('#srw_tipo_consulta')
-        .select('Consulta médica');
-      cy.get('#ptd_rut')
-        .type('18376588-4');
-      cy.get('#srw_num_boleta')
-        .type('123');
-      cy.get('#continuar_datos')
-        .click();
-      cy.get('#srw_rut_tratante')
-        .type('18376588-4');
-      cy.get('#srw_nom_tratante')
-        .type('Sebastián Carrasco Poblete')
-      cy.get('#archivo').selectFile('DEJAR_BOLETAS/json.json', { force: true });
-      cy.get('#archivo_oa').selectFile('DEJAR_BOLETAS/json.json', { force: true });
-      cy.get('#chk_verificacion')
-        .click();
+      cy.task('obtenerArchivosEnCarpeta', 'DEJAR_BOLETAS').then((archivos) => {
+        archivos.forEach((archivo) => {
+          cy.log(`Procesando ${archivo}`);
+
+          cy.visit('https://sv.nuevamasvida.cl/sucursal_virtual/solicitud_reembolso.php');
+          cy.get('#chk_verificacion')
+            .click();
+          cy.get('#activo > img')
+            .click();
+          cy.get('#bnf_rut')
+            .select(Cypress.env('NOMBRE'));
+          cy.get('#srw_tipo_consulta')
+            .select('Consulta médica');
+          cy.get('#ptd_rut')
+            .type('18376588-4');
+          cy.get('#srw_num_boleta')
+            .type('123');
+          cy.get('#continuar_datos')
+            .click();
+          cy.get('#srw_rut_tratante')
+            .type('18376588-4');
+          cy.get('#srw_nom_tratante')
+            .type('Sebastián Carrasco Poblete')
+
+          cy.get('#archivo').selectFile(`DEJAR_BOLETAS/${archivo}`, { force: true });
+          cy.get('#archivo_oa').selectFile(`DEJAR_BOLETAS/${archivo}`, { force: true });
+          cy.get('#chk_verificacion')
+            .click();
+        });
+      });
     });
   });
 });
